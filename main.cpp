74ce4de717309b54cbfda2ec692a9bac7b0c31f5
yang <907616004@qq.com>
2023-11-27 23:58:23 +0800

    update paddleocr==2.12

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include "yolov7/yolov7-tiny-480-paddle-lite.cpp"
+#include "paddle_ocr/paddle_ocr_v4_paddle_lite.cpp"
 
 int main() {
-    test_yolov7_tiny_480_paddle_lite();
+//    test_yolov7_tiny_480_paddle_lite();
+    test_paddle_ocr_v4_paddle_lite();
     return 0;
 }
