ad1ea9cacdc49333b4f79ba06a9be3d5ed56af30
yang <yangcheng@zuzuche.com>
2023-12-06 17:58:13 +0800

    调通paddleocr,rec部分需要在循环里面每次都初始化predictor,不然有bug有点奇怪.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include "yolov7/yolov7-tiny-480-paddle-lite.cpp"
-#include "paddle_ocr/paddle_ocr_v4_paddle_lite.cpp"
+#include "paddle_ocr/paddle_ocr_v4_paddle_lite2.cpp"
 
 int main() {
 //    test_yolov7_tiny_480_paddle_lite();
     test_paddle_ocr_v4_paddle_lite();
+//    test_paddle_ocr_v4_rec();
     return 0;
 }
